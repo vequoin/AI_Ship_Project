ce551987f5121588ac7e65cba0f3b6b823bdb51c
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-10-09 17:06:48 -0400

    Update main.py

--- ProjectAI/main.py
+++ ProjectAI/main.py
@@ -1,19 +1,30 @@
 from Ship import Ship
 from fireburning_sim import Fire
 from bot import Bot
+from GameManager import GameManager
 import random
-from jiji import 
+
+get_ship_size = 25
+q = .5
+bot_strategy = "7"
 
 def main():
     '''get_ship_size = int(input("Please enter your ship size: "))
     q = float(input("Enter the probability of fire:  "))
-    bot_strategy = input("Please select a Strategy: \n Stategy 1) \t Strategy 2) \n Strategy 3) Strategy 4)")
-    game = GameManager(get_ship_size, q, bot_strategy)
-    game.runGame()
-    print(game.ship.name)
-    print(game.ship)'''
-    ship = Ship(10)
-    print(ship)
-
-if __name__ == "__main__":
-    main()
+    bot_strategy = input("Enter a strategy: ")'''
+    wins = 0
+    losses = 0
+    wins_shortest_path = 0
+    loss_shortest_path = 0
+    for i in range(20):
+        game = GameManager(get_ship_size, q, bot_strategy)
+        verdict = game.runGame()
+        if verdict == "W":
+            wins += 1
+        elif verdict == "T":
+            wins_shortest_path += 1
+        elif verdict == "S":
+            loss_shortest_path += 1
+        else:
+            losses += 1
+    print(f"Losses: {losses} and Wins: {wins} ShortestPath: {wins_shortest_path}  LossesSP: {loss_shortest_path}")
